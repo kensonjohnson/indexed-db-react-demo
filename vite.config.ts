1ecdb454202617573f6b36c250cbb50ab7b0de1c
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2025-08-14 13:22:16 -0500

    Set base to repo during build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const base =
+  process.env.NODE_ENV === "production" ? "indexed-db-react-demo" : "/";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base,
 });

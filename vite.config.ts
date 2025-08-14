8f2fd6490dc6a43be005067eb2f3f5afa13e80c4
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2025-08-14 13:26:19 -0500

    Add slash prefix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 const base =
-  process.env.NODE_ENV === "production" ? "indexed-db-react-demo" : "/";
+  process.env.NODE_ENV === "production" ? "/indexed-db-react-demo" : "/";
 
 // https://vite.dev/config/
 export default defineConfig({
